4a9833a3e97e2c8fe0639bcdf0250f4638e28cd9
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-22 13:55:37 -0500

    commit tree eeb2011e9097c2aca5ae4bab0ee5b9ebc7dd9c01

---
+++ lib/student.rb
@@ -0,0 +1,12 @@
+require 'pry'
+class Student
+
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand(num = 1)
+        puts "Pick me!\n" * num
+    end
+
+end
